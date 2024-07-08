72d9d7d47b6cd1e191f54cd21054cd8aac3722c3
Misaki <64830812+misaki-web@users.noreply.github.com>
2024-07-07 20:29:05 -0400

    v1.0.1

--- acf-json-field.php
+++ acf-json-field.php
@@ -5,7 +5,7 @@
  * Description: A custom ACF field type for manipulating JSON data
  * Text Domain: acf-json-field
  * Author: Misaki F.
- * Version: 1.0.0
+ * Version: 1.0.1
  */
 
 namespace AcfJsonField;
@@ -18,7 +18,7 @@ if (!defined('ABSPATH')) {
 # @title Constants
 ################################################################################
 
-define('ACF_JSON_FIELD_VERSION', '1.0.0');
+define('ACF_JSON_FIELD_VERSION', '1.0.1');
 
 ################################################################################
 # @title Inclusions
